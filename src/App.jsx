3f3bde408c15c80bf96a845726287ca9bc062e9e
rajankumar-dev <rk1620703@gmail.com>
2025-08-05 22:21:37 +0530

    commit in Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Support from "./pages/Support/Support";
+
 import TransactionPage from "./pages/Transaction/Transaction";
 const router = createBrowserRouter([
+
   {
     path: "/",
     element: <Dashboard />,
